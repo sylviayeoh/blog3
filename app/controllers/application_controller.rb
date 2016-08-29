c44ad8721ad0b584a87b1a3934356d3aa7642b0f
Sylvia Yeoh <sylviayeohyt@gmail.com>
2016-08-29 16:29:00 +0800

    image and colour

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+http_basic_authenticate_with name: "sylvia", password: "secret", except: [:index, :show, :about]
+
   protect_from_forgery with: :exception
 end
